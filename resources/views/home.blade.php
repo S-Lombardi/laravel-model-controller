6c3a397df22d5c40199f737ccf07bae2f089f6b0
S-Lombardi <silo.lombardi@gmail.com>
2023-07-25 18:05:51 +0200

    Aggiunti tag li per visualizzare le info dei fumetti

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,15 +4,24 @@
     <div class="container">
         <div class="row justify-content-center p-4">
             @foreach($comics as $comic)
-            <div class="card col-12 col-md-3 p-4 mb-5 me-3">
+            <ul class="card col-12 col-md-3 p-4 mb-5 me-3 list-unstyled">
                 {{-- Immagine --}}
-                <img src="{{$comic->thumb}}" alt="">
+                <li>
+                    <img src="{{$comic->thumb}}" alt="">
+                </li>
                 {{-- Titolo --}}
-                <h4 class="mt-3">{{$comic->title}}</h4>
-
+                <li>
+                    <h5 class="mt-3 text-uppercase">{{$comic->title}}</h5>
+                </li>
+                {{-- Tipo --}}
+                <li class="fst-italic">
+                    {{$comic->type}}
+                </li>
                 {{-- Prezzo --}}
-                <div>Prezzo:{{$comic->price}}</div>
-            </div>
+                <li>
+                    Prezzo: {{$comic->price}}
+                </li>
+            </ul>
             @endforeach
         </div>
     </div>
