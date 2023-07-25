4c1f8f23c113d38a3f32ccc153e91cb3655e57c9
S-Lombardi <silo.lombardi@gmail.com>
2023-07-25 15:53:59 +0200

    creato file app.bLADE

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,24 +1,12 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+@extends('layouts.app')
 
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+@section('content')
+<div class="container">
+    <div class="row">
 
-    <title>Laravel model controller</title>
+        Lavarel model controller
+    </div>
+</div>
+@endsection
 
-    <!-- Fonts -->
-    <link href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
 
-    <!-- Styles -->
-    @vite('resources/js/app.js')
-
-</head>
-
-<body>
-
-    Lavarel model controller
-
-</body>
-
-</html>
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\Guest\PageController as PageController;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -13,6 +15,5 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('home');
-});
+//ROTTE CON CONTROLLER
+Route::get('/', [PageController::class,'index']);
