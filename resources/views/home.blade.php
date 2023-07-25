441962219420b8b3f2a8af22ee517779ba6706fa
S-Lombardi <silo.lombardi@gmail.com>
2023-07-25 17:45:23 +0200

    modificate card con stili di bootstrap

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,19 +2,18 @@
 
 @section('content')
     <div class="container">
-        <div class="row">
-                @foreach($comics as $comic)
-                <div class="card col-12 col-md-6 col-lg-4">
-                    
-                    <h4> {{$comic->title}}</h4>
+        <div class="row justify-content-center p-4">
+            @foreach($comics as $comic)
+            <div class="card col-12 col-md-3 p-4 mb-5 me-3">
+                {{-- Immagine --}}
+                <img src="{{$comic->thumb}}" alt="">
+                {{-- Titolo --}}
+                <h4 class="mt-3">{{$comic->title}}</h4>
 
-                    <img src="{{$comic->thumb}}" alt="">
-
-                </div>
-                @endforeach
-
-            
-            
+                {{-- Prezzo --}}
+                <div>Prezzo:{{$comic->price}}</div>
+            </div>
+            @endforeach
         </div>
     </div>
 @endsection
